7a4044985767ff5d97bfe88c12f61be2d7ec8c38
NicolasAc <acosta.nicolas@hotmail.es>
2025-05-09 22:11:33 -0300

    Rama Login1

--- src/main/java/org/cnasm/Sisem/SisemApplication.java
+++ src/main/java/org/cnasm/Sisem/SisemApplication.java
@@ -12,7 +12,7 @@ public class SisemApplication {
 		SpringApplication.run(SisemApplication.class, args);
 
 		// Solo para generar una contraseña encriptada y copiarla desde log
-		String rawPassword = "admin"; // ramaLoginm
+		String rawPassword = "admin"; // ramaLoginm1
 		String hash = new BCryptPasswordEncoder().encode(rawPassword);
 		System.out.println("Hashed password: " + hash);
 	}
